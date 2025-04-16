ab511453bdca3f7a1cd3656aaf99543f556330d8
Mz-cell <andrew.martinez@weezie.io>
2025-04-16 15:44:29 +0100

    1.commit

--- JS/profile.js
+++ JS/profile.js
@@ -112,7 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
   saveBtn.addEventListener("click", () => {
     const selectedPhoto = mainPhoto.getAttribute("src");
     localStorage.setItem("selectedPhoto", selectedPhoto);
-    alert(`✅Gguardado correctamente.`);
+    alert(`✅Guardado correctamente.`);
     location.reload();
   });
 });
